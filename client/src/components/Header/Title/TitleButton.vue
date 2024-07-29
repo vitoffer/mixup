a3d8ad00478bd0217dceab9f5366d0ade4b39b48
vitoffer <ms.titoff1011@yandex.ru>
2024-07-30 02:19:49 +0300

    refactor header components

--- client/src/components/Header/Title/TitleButton.vue
+++ client/src/components/Header/Title/TitleButton.vue
@@ -5,8 +5,6 @@ import { PlayIcon } from "@heroicons/vue/24/solid"
 <template>
 	<button class="button button--red button--solid">
 		<PlayIcon style="width: 16px; color: var(--bg-color)" />
-		<span class="font-open-sans line text-[12px] font-bold leading-[16px]">
-			Бот в дискорде
-		</span>
+		<span class="font-bold">Бот в дискорде</span>
 	</button>
 </template>
