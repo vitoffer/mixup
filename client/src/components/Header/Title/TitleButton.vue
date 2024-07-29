9b9af047db6bfa4a65959a51d0be069968c5d320
vitoffer <ms.titoff1011@yandex.ru>
2024-07-30 01:25:46 +0300

    change title button classes

--- client/src/components/Header/Title/TitleButton.vue
+++ client/src/components/Header/Title/TitleButton.vue
@@ -3,9 +3,7 @@ import { PlayIcon } from "@heroicons/vue/24/solid"
 </script>
 
 <template>
-	<button
-		class="bg-red hover:text-red hover:border-red flex items-center justify-center gap-[5px] rounded-[10px] border border-solid border-transparent px-[27px] py-[5px] hover:bg-opacity-0"
-	>
+	<button class="button button--red button--solid">
 		<PlayIcon style="width: 16px; color: var(--bg-color)" />
 		<span class="font-open-sans line text-[12px] font-bold leading-[16px]">
 			Бот в дискорде
