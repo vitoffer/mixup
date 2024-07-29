1b60c0b15734eeec5897900004f82f83ea9bf952
vitoffer <ms.titoff1011@yandex.ru>
2024-07-30 01:25:17 +0300

    add button and header-action utility classes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,28 +2,30 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
 	theme: {
-		extend: {},
-		colors: {
-			gray: {
-				DEFAULT: "#0d1f2d",
-				light: "#253542",
+		extend: {
+			colors: {
+				gray: {
+					DEFAULT: "#0d1f2d",
+					light: "#253542",
+				},
+				yellow: {
+					DEFAULT: "#ffc53a",
+					light: "#ffe29d",
+				},
+				red: "#f25445",
+				cyan: {
+					DEFAULT: "#79aea3",
+					light: "#a1c6bf",
+				},
+				transparent: "transparent",
 			},
-			yellow: {
-				DEFAULT: "#ffc53a",
-				light: "#ffe29d",
+			fontFamily: {
+				primary: "Open Sans",
+				accent: "Nunito Sans 10pt",
+				holtwood: "Holtwood One SC",
 			},
-			red: "#f25445",
-			cyan: {
-				DEFAULT: "#79aea3",
-				light: "#a1c6bf",
-			},
-			transparent: "transparent",
-		},
-		fontFamily: {
-			primary: "Open Sans",
-			accent: "Nunito Sans 10pt",
-			holtwood: "Holtwood One SC",
 		},
 	},
+
 	plugins: [],
 }
