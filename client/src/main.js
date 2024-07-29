b3a4db7f5334c87c82d994b96838b82c4d856ae7
vitoffer <ms.titoff1011@yandex.ru>
2024-07-29 19:03:11 +0300

    reset css styles

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
+
+import "./styles/reset.css"
+
+import "./assets//fonts/open_sans/stylesheet.css"
+import "./assets/fonts/holtwood_one_sc/stylesheet.css"
+import "./assets/fonts/nunito_sans/stylesheet.css"
 
 const app = createApp(App)
 
-app.mount('#app')
+app.mount("#app")
