05cbbbe7980d3570b4618c07ae994ebacdb4eec7
vitoffer <ms.titoff1011@yandex.ru>
2024-07-30 14:20:05 +0300

    fix track list layout

--- client/src/components/TrackList/TrackCard.vue
+++ client/src/components/TrackList/TrackCard.vue
@@ -11,15 +11,19 @@ defineProps(["track"])
 			:src="$props.track.image"
 			alt="Card image"
 		/>
-		<div class="mb-[6px] flex flex-col gap-[4px]">
-			<h2 class="line-clamp-2 text-[16px] text-yellow">
+		<div class="flex flex-col gap-[2px]">
+			<h2 class="line-clamp-2 text-[16px] leading-[22px] text-yellow">
 				{{ $props.track.title }}
 			</h2>
-			<p class="line-clamp-1 font-accent text-[12px] text-yellow-light">
+			<p
+				class="line-clamp-1 font-accent text-[12px] leading-[16px] text-yellow-light"
+			>
 				by {{ $props.track.author }}
 			</p>
 		</div>
-		<ul class="w-[100%] list-inside list-disc text-[12px] text-cyan">
+		<ul
+			class="w-[100%] list-inside list-disc text-[12px] leading-[16px] text-cyan"
+		>
 			<li
 				class="overflow-hidden text-ellipsis whitespace-nowrap"
 				v-for="trackInside in $props.track.tracksInside"
