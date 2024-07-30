7729d48bcdb44da4aa20b7d1241c368bd9f94aac
vitoffer <ms.titoff1011@yandex.ru>
2024-07-30 14:14:11 +0300

    start track list layout

--- client/src/components/TheTrackList.vue
+++ client/src/components/TheTrackList.vue
@@ -1,7 +1,59 @@
 <script setup>
 import TrackCard from "./TrackList/TrackCard.vue"
+
+import image1Url from "@/assets/dev/card_image_1.jpg"
+import image2Url from "@/assets/dev/card_image_2.jpg"
+import image3Url from "@/assets/dev/card_image_3.jpg"
+import image4Url from "@/assets/dev/card_image_4.jpg"
+import image5Url from "@/assets/dev/card_image_5.jpg"
+
+const trackList = [
+	{
+		id: 1,
+		image: image1Url,
+		title: "Биг Сити Лайф - Whole Lotta Swag",
+		author: "author 1",
+		tracksInside: ["Opsss 1 - Kent 1", "Track 1 - Lorem ipsum dolor sit."],
+	},
+	{
+		id: 2,
+		image: image2Url,
+		title: "Vmeste My - 5sta Family, 6YNTHMANE, budAGE",
+		author: "author 1",
+		tracksInside: ["Track 1 - Kent 1", "Gooop - Lorem ipsum dolor sit amet."],
+	},
+	{
+		id: 3,
+		image: image3Url,
+		title: "Supersonic - Big Baby Tape, Aarne",
+		author: "author 2",
+		tracksInside: ["Track 2 - Kent 2", "Track 22 - SSSAAA"],
+	},
+	{
+		id: 4,
+		image: image4Url,
+		title: "Музыка громче - BUSHIDO ZHO",
+		author: "author 4",
+		tracksInside: ["Track 4 - Kent 1", "Track 2 - Kiii"],
+	},
+	{
+		id: 5,
+		image: image5Url,
+		title: "далеко (большой Бушизм) [prod. by wex & heysubr] - BUSHIDO ZHO",
+		author: "author 5",
+		tracksInside: ["Track 5 - Kent 5", "Track 2 - Sawdadwadnfa ahsh"],
+	},
+]
 </script>
 
 <template>
-	<TrackCard />
+	<main class="m-auto flex justify-center">
+		<div class="grid grid-cols-[repeat(4,_min-content)] gap-[24px]">
+			<TrackCard
+				v-for="track in trackList"
+				:key="track.id"
+				:track="track"
+			/>
+		</div>
+	</main>
 </template>
--- client/src/components/TrackList/TrackCard.vue
+++ client/src/components/TrackList/TrackCard.vue
@@ -1,19 +1,32 @@
+<script setup>
+defineProps(["track"])
+</script>
+
 <template>
 	<div
-		class="shadow-card mx-[16px] flex h-[413px] w-[260px] flex-col items-center gap-[8px] rounded-[20px] bg-gray p-[16px] text-center"
+		class="flex w-[260px] flex-col items-center gap-[8px] rounded-[20px] bg-gray p-[16px] text-center shadow-card"
 	>
 		<img
 			class="w-[228px]"
-			src="@/assets/dev/card_image_1.jpg"
+			:src="$props.track.image"
 			alt="Card image"
 		/>
-		<div class="flex flex-col gap-[2px]">
-			<h2 class="text-[16px] text-yellow">Биг Сити Лайф - Whole Lotta Swag</h2>
-			<p class="font-accent text-[12px] text-yellow-light">by author 3</p>
+		<div class="mb-[6px] flex flex-col gap-[4px]">
+			<h2 class="line-clamp-2 text-[16px] text-yellow">
+				{{ $props.track.title }}
+			</h2>
+			<p class="line-clamp-1 font-accent text-[12px] text-yellow-light">
+				by {{ $props.track.author }}
+			</p>
 		</div>
-		<ul class="list-inside list-disc text-[12px] text-cyan">
-			<li>Future 1 - Kent 1</li>
-			<li>Track 2 - Sawdadwadnfa ahsh1</li>
+		<ul class="w-[100%] list-inside list-disc text-[12px] text-cyan">
+			<li
+				class="overflow-hidden text-ellipsis whitespace-nowrap"
+				v-for="trackInside in $props.track.tracksInside"
+				:key="trackInside"
+			>
+				{{ trackInside }}
+			</li>
 		</ul>
 		<button class="button button--red button--outline">
 			Трек в дискорд боте
