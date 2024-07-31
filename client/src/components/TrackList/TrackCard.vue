872e8fd4a5e8d294ec6a78a371be86f35f4dd1ea
vitoffer <ms.titoff1011@yandex.ru>
2024-07-31 19:07:00 +0300

    complete show full text widget

--- client/src/components/TrackList/TrackCard.vue
+++ client/src/components/TrackList/TrackCard.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 	const ul = document.getElementById(`${props.track.id}-ul`)
 	let lis = ul.children
 
-	if (countLines(title) == 1) {
+	if (linesCount(title) == 1) {
 		ul.style.marginTop = "auto"
 		ul.style.marginBottom = "16px"
 	} else {
@@ -59,64 +59,155 @@ function isClamped(e) {
 	return e.scrollHeight - e.clientHeight > 5
 }
 
-function countLines(el) {
-	var divHeight = el.offsetHeight
+function linesCount(el) {
+	const divHeight = el.clientHeight
 
 	const lineHeight = parseFloat(window.getComputedStyle(el).lineHeight)
 
-	var lines = Math.round(divHeight / lineHeight)
+	const lines = Math.round(divHeight / lineHeight)
 	return lines
 }
 
-function mouseOverTitle(e) {
-	if (!isClamped(e.target)) {
+// function mouseOverTitle(e) {
+// 	if (!isClamped(e.target)) {
+// 		return
+// 	}
+
+// 	const el = e.target
+// 	el.classList.add("expanded")
+// 	const elWidth = el.clientWidth
+// 	const elHeight = el.clientHeight
+
+// 	el.style.overflow = "visible"
+// 	el.style.position = "absolute"
+
+// 	el.style.width = "150%"
+// 	el.style.borderRadius = "10px"
+// 	el.style.left = "-25%"
+// 	el.style.boxShadow = "0 0 6px 0 rgba(0 0 0 / 10%)"
+
+// 	el.classList.add("bg-gray-light")
+
+// 	if (el.nextSibling.nodeName == "DIV") {
+// 		el.nextSibling.style.display = ""
+// 	} else {
+// 		const emptyEl = document.createElement("div")
+// 		emptyEl.style.width = `${elWidth}px`
+// 		emptyEl.style.height = `${elHeight - 0.2}px`
+
+// 		el.after(emptyEl)
+// 	}
+// }
+
+// function mouseOutTitle(e) {
+// 	const el = e.target
+// 	if (!el.classList.contains("expanded")) {
+// 		return
+// 	}
+
+// 	el.style.overflow = "hidden"
+// 	el.style.position = ""
+// 	el.style.width = ""
+// 	el.classList.remove("bg-gray-light")
+
+// 	el.nextSibling.style.display = "none"
+// }
+
+function showFullText(event) {
+	const el = event.target
+
+	if (
+		(linesCount(el) >= 2 && !isClamped(el)) ||
+		(linesCount(el) == 1 &&
+			el.classList.contains("author") &&
+			!isClamped(el)) ||
+		(linesCount(el) == 1 &&
+			!el.classList.contains("author") &&
+			!isEllipsisActive(el))
+	) {
 		return
 	}
 
-	const el = e.target
-	el.classList.add("expanded")
 	const elWidth = el.clientWidth
 	const elHeight = el.clientHeight
 
-	el.style.overflow = "visible"
-	el.style.position = "absolute"
+	const { top: elTop } = getCoords(el)
+	const { top: cardTop } = getCoords(el.closest(".card"))
 
-	el.style.width = "150%"
-	el.style.borderRadius = "10px"
-	el.style.left = "-25%"
-	el.style.boxShadow = "0 0 6px 0 rgba(0 0 0 / 10%)"
-
-	el.classList.add("bg-gray-light")
-
-	if (el.nextSibling.nodeName == "DIV") {
-		el.nextSibling.style.display = ""
+	if (el.nextSibling?.classList.contains("empty-el")) {
+		el.nextSibling.style.display = "block"
 	} else {
 		const emptyEl = document.createElement("div")
+
 		emptyEl.style.width = `${elWidth}px`
 		emptyEl.style.height = `${elHeight - 0.2}px`
+		emptyEl.classList.add("empty-el")
 
 		el.after(emptyEl)
 	}
+
+	el.classList.add(
+		"expanded",
+		"bg-gray-light",
+		linesCount(el) == 1 ? "rounded-[5px]" : "rounded-[10px]",
+		"absolute",
+		"p-[5px]",
+	)
+
+	el.style.cssText = `
+	z-index: 1;
+	width: 384px;
+	top: ${elTop - cardTop - 5}px;
+	left: -48px;
+	box-shadow: 0 0 4px 0 rgba(0 0 0 / 10%);
+	`
+
+	el.classList.remove("line-clamp-1", "line-clamp-2")
 }
 
-function mouseOutTitle(e) {
-	const el = e.target
+function hideFullText(event) {
+	const el = event.target
+
 	if (!el.classList.contains("expanded")) {
 		return
 	}
 
-	el.style.overflow = "hidden"
-	el.style.position = ""
-	el.style.width = ""
-	el.classList.remove("bg-gray-light")
+	el.style.width = "initial"
+	el.classList.remove(
+		"bg-gray-light",
+		"absolute",
+		"rounded-[10px]",
+		"expanded",
+		"p-[5px]",
+	)
 
-	el.nextSibling.style.display = "none"
+	if (el.classList.contains("author")) {
+		el.classList.add("line-clamp-1")
+	}
+	if (el.classList.contains("title")) {
+		el.classList.add("line-clamp-2")
+	}
+
+	if (el.nextSibling.classList.contains("empty-el")) {
+		el.nextSibling.style.display = "none"
+	}
+}
+
+function getCoords(elem) {
+	let box = elem.getBoundingClientRect()
+
+	return {
+		top: box.top + window.scrollY,
+		right: box.right + window.scrollX,
+		bottom: box.bottom + window.scrollY,
+		left: box.left + window.scrollX,
+	}
 }
 </script>
 
 <template>
 	<div
-		class="flex flex-col items-center rounded-[20px] bg-gray p-[16px] text-center shadow-card"
+		class="card relative flex flex-col items-center rounded-[20px] bg-gray p-[16px] text-center shadow-card"
 	>
 		<img
 			:src="$props.track.image"
@@ -124,18 +215,20 @@ function mouseOutTitle(e) {
 			width="256"
 			class="mb-[8px]"
 		/>
-		<div class="relative flex flex-col gap-[4px]">
+		<div class="flex flex-col gap-[4px]">
 			<h2
-				class="line-clamp-2 text-[18px] leading-[1.3] text-yellow"
+				class="title line-clamp-2 text-[18px] leading-[1.3] text-yellow"
 				ref="el"
 				:id="`${$props.track.id}-h2`"
-				@mouseover="mouseOverTitle"
-				@mouseout="mouseOutTitle"
+				@mouseover="showFullText"
+				@mouseout="hideFullText"
 			>
 				{{ $props.track.title }}
 			</h2>
 			<p
-				class="line-clamp-1 font-accent text-[16px] leading-[1.3] text-yellow-light"
+				class="author line-clamp-1 font-accent text-[16px] leading-[1.3] text-yellow-light"
+				@mouseover="showFullText"
+				@mouseout="hideFullText"
 			>
 				by {{ $props.track.author }}
 			</p>
