0c6e76806532f336ae419269f908ab3d0cfbf206
vitoffer <ms.titoff1011@yandex.ru>
2024-08-02 21:38:12 +0300

    fix server error with env vars

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 const Track = require("./models/track")
 
+require("dotenv").config()
+
 const app = express()
 
 app.use(bodyParser.json())
