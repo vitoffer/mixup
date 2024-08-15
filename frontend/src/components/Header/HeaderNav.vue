c77781e8aceffdee170bf0909a5ee790440facd6
vitoffer <ms.titoff1011@yandex.ru>
2024-08-15 13:03:23 +0300

    add create mix component and route for it

--- frontend/src/components/Header/HeaderNav.vue
+++ frontend/src/components/Header/HeaderNav.vue
@@ -3,35 +3,12 @@ const navItems = [
 	{
 		name: "Все треки",
 		link: "/",
-		isEnabled: true,
 	},
 	{
-		name: "Кураторы",
-		link: "/404",
-
-		isEnabled: false,
-	},
-	{
-		name: "Добавить трек",
-		link: "/404",
-		isEnabled: false,
+		name: "Добавить микс",
+		link: "/create-mix",
 	},
 ]
-
-function generateStyles(isEnabled) {
-	if (isEnabled) {
-		return {
-			fontWeight: "bold",
-			color: "var(--cyan-500)",
-			textDecoration: "underline",
-		}
-	}
-	return {
-		fontWeight: 500,
-		color: "var(--cyan-700)",
-		textDecoration: "line-through",
-	}
-}
 </script>
 
 <template>
@@ -39,22 +16,14 @@ function generateStyles(isEnabled) {
 		<ul class="nav__list">
 			<li
 				class="nav__item"
-				v-for="{ name, link, isEnabled } in navItems"
+				v-for="{ name, link } in navItems"
 				:key="name"
 			>
 				<RouterLink
 					:to="link"
 					class="nav__link"
-					:class="isEnabled ? '' : 'hover-not-allowed'"
-					:style="[generateStyles(isEnabled), { position: 'relative' }]"
 				>
 					<h2>{{ name }}</h2>
-					<div
-						class="tooltip"
-						v-if="!isEnabled"
-					>
-						В разработке
-					</div>
 				</RouterLink>
 			</li>
 		</ul>
@@ -81,45 +50,18 @@ function generateStyles(isEnabled) {
 .nav__link {
 	line-height: 25px;
 	padding-block: 8px;
+	font-weight: 500;
+	color: var(--cyan-700);
 }
 
-.hover-not-allowed:hover {
-	cursor: not-allowed;
+.nav__link:hover {
+	color: var(--cyan-500);
+	text-decoration: underline;
 }
 
-.tooltip {
-	position: absolute;
-	top: 90%;
-	left: 50%;
-	translate: -50% 0;
-	z-index: 1;
-	opacity: 0;
-
-	width: 130px;
-
-	padding: 5px;
-	background-color: var(--gray-700);
-	border-radius: 10px;
-	color: var(--yellow-700);
-	text-align: center;
-	transition: 200ms;
-}
-
-.tooltip::after {
-	content: " ";
-	position: absolute;
-	bottom: 100%;
-	left: 50%;
-	translate: 0 1px;
-	z-index: 2;
-	margin-left: -5px;
-	border-width: 5px;
-
-	border-style: solid;
-	border-color: transparent transparent var(--gray-700) transparent;
-}
-
-.nav__link:hover .tooltip {
-	opacity: 1;
+.nav__link.router-link-exact-active {
+	font-weight: bold;
+	color: var(--cyan-500);
+	text-decoration: underline;
 }
 </style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router"
 
 import MainView from "@/views/MainView.vue"
 import TrackView from "@/views/TrackView.vue"
+import CreateMixView from "@/views/CreateMixView.vue"
 import NotFoundView from "@/views/NotFoundView.vue"
 
 const routes = [
 	{ path: "/", component: MainView },
 	{ path: "/tracks/:id", component: TrackView },
+	{ path: "/create-mix", component: CreateMixView },
 	{ path: "/404", component: NotFoundView },
 ]
 
