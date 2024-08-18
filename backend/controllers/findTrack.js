1c513be504dfb164bec8578b8141cdeccb163e44
vitoffer <ms.titoff1011@yandex.ru>
2024-08-19 00:12:04 +0300

    add search track on spotify

--- backend/controllers/findTrack.js
+++ backend/controllers/findTrack.js
@@ -1,5 +1,6 @@
 import youtubeSearch from "youtube-search"
-import { YOUTUBE_API_KEY } from "../constants.js"
+import { YOUTUBE_API_KEY, SPOTIFY_ID, SPOTIFY_SECRET } from "../constants.js"
+import SpotifyWebApi from "spotify-web-api-node"
 
 export function findTrackOnYoutube(req, res) {
 	const { name, authors } = req.body
@@ -38,7 +39,46 @@ export function findTrackOnYoutube(req, res) {
 	)
 }
 
-export function findTrackOnSpotify(req, res) {}
+export async function findTrackOnSpotify(req, res) {
+	const { name: searchName, authors: searchAuthors } = req.body
+
+	const spotifyApi = new SpotifyWebApi({
+		clientId: SPOTIFY_ID,
+		clientSecret: SPOTIFY_SECRET,
+	})
+
+	try {
+		const data = await spotifyApi.clientCredentialsGrant()
+
+		const token = data.body["access_token"]
+		const expiresIn = data.body["expires_in"]
+
+		// console.log("The access token expires in " + expiresIn)
+		// console.log("The access token is " + token)
+
+		spotifyApi.setAccessToken(token)
+	} catch (err) {
+		console.log("Something went wrong when retrieving an access token", err)
+	}
+
+	const results = await spotifyApi.searchTracks(
+		`${searchName} ${searchAuthors ? searchAuthors.join(", ") : ""}`,
+		{ limit: 5 }
+	)
+
+	const tracks = results.body.tracks.items
+
+	const resultTracks = tracks.map((track) => {
+		return {
+			name: track.name,
+			authors: track.artists.map((artist) => artist.name),
+			link: track.external_urls.spotify,
+			imageUrl: track.album.images[1].url,
+		}
+	})
+
+	res.send(resultTracks)
+}
 
 export function findTrackOnVK(req, res) {}
 
