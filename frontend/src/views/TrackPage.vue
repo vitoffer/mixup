696f4974f94e7e4795fb16ceaef5091a8ef7f8a5
vitoffer <ms.titoff1011@yandex.ru>
2024-08-27 22:24:26 +0300

    add useTrackPage composable

--- frontend/src/views/TrackPage.vue
+++ frontend/src/views/TrackPage.vue
@@ -1,29 +1,10 @@
 <script setup>
-import { ref, watch } from "vue"
-import { useRoute } from "vue-router"
 import BaseLayout from "@/components/BaseLayout.vue"
 import TrackInfo from "@/components/track-page/TrackInfo.vue"
 import MixedTrackList from "@/components/track-page/MixedTrackList.vue"
-import { findTrack, loadTrack } from "@/modules/trackPage"
+import { useTrackPage } from "@/composables/trackPage"
 
-const track = ref(null)
-const isTrackLoading = ref(false)
-
-const route = useRoute()
-
-watch(
-	() => route.params.id,
-	async () => {
-		track.value = findTrack(route.params.id)
-
-		if (!track.value) {
-			isTrackLoading.value = true
-			track.value = await loadTrack(route.params.id)
-			isTrackLoading.value = false
-		}
-	},
-	{ immediate: true },
-)
+const { track, isTrackLoading } = useTrackPage()
 </script>
 
 <template>
