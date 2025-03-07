30032daafdde75b3de8af0277d9126ac0a96448a
vitoffer <ms.titoff1011@yandex.ru>
2025-03-07 08:10:13 +0300

    add basic dockerfiles and configs for ci/cd (need fixes)

--- backend-hono/src/index.ts
+++ backend-hono/src/index.ts
@@ -6,4 +6,7 @@ app.get("/", (c) => {
 	return c.text("Hello Hono!")
 })
 
-export default app
+export default {
+	port: 4000,
+	fetch: app.fetch,
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,8 +11,4 @@ export default defineConfig({
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
 	},
-	build: {
-		outDir: "../dist",
-		emptyOutDir: true,
-	},
 })
