a5fee92725b2e2872702825a1cd11828f68b0c80
Mikhail <ms.titoff1011@yandex.ru>
2025-03-07 20:48:40 +0300

    move nginx docker to frontend and configured container

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,28 @@
 <script setup>
 import { RouterView } from "vue-router"
 import TheHeader from "@/components/TheHeader.vue"
+import { ref } from "vue"
+import { onMounted } from "vue"
+
+const pinged = ref(false)
+const err = ref("")
+
+const ping = async () => {
+	try {
+		const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/ping`)
+		pinged.value = true
+	} catch (e) {
+		err.value = e
+	}
+}
+
+onMounted(ping)
 </script>
 
 <template>
-	<TheHeader />
-	<RouterView />
+	{{ pinged }}
+	<br />
+	{{ err }}
+	<!-- <TheHeader />
+	<RouterView /> -->
 </template>
