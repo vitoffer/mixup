0512cfb2c5e1c69e244b281a7d1c2982c0ba0e1d
Mikhail <ms.titoff1011@yandex.ru>
2025-03-07 20:48:11 +0300

    configured backend docker

--- backend-hono/src/index.ts
+++ backend-hono/src/index.ts
@@ -1,12 +1,20 @@
 import { Hono } from "hono"
+import { logger } from "hono/logger"
 
 const app = new Hono()
+
+app.use(logger())
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!")
 })
 
+app.get("/ping", (c) => {
+	return c.text("pong!")
+})
+
 export default {
 	port: 4000,
+	host: "0.0.0.0",
 	fetch: app.fetch,
 }
