b4bef543442c578007fb49e2b2aa5f811bd1a2cb
Mikhail <ms.titoff1011@yandex.ru>
2025-03-07 19:45:43 +0300

    change yarn to pnpm in frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue"
 import PrimeVue from "primevue/config"
 import Tooltip from "primevue/tooltip"
-import Aura from "@primevue/themes/aura"
+import Aura from "@primeuix/themes/aura"
 import App from "./App.vue"
 import { router } from "@/router/router"
 
