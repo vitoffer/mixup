12840186004e59e485e9f9ff10f1605f23437d72
vitoffer <ms.titoff1011@yandex.ru>
2025-03-08 18:16:52 +0300

    add stoker utils

--- backend-hono/src/lib/createApp.ts
+++ backend-hono/src/lib/createApp.ts
@@ -1,9 +1,11 @@
 import { OpenAPIHono } from "@hono/zod-openapi"
 import { cors } from "hono/cors"
 import { logger } from "hono/logger"
+import { notFound, onError } from "stoker/middlewares"
+import { defaultHook } from "stoker/openapi"
 
 export function createRouter() {
-	return new OpenAPIHono({ strict: false })
+	return new OpenAPIHono({ strict: false, defaultHook })
 }
 
 export default function createApp() {
@@ -11,6 +13,9 @@ export default function createApp() {
 
 	app.use(logger())
 	app.use("*", cors())
+
+	app.notFound(notFound)
+	app.onError(onError)
 
 	return app
 }
