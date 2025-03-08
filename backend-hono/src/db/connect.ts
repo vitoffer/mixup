aa4e3f854b432b477da628b25a90d49e8c5c2756
vitoffer <ms.titoff1011@yandex.ru>
2025-03-08 17:15:38 +0300

    encode username and password in connect db uri

--- backend-hono/src/db/connect.ts
+++ backend-hono/src/db/connect.ts
@@ -2,8 +2,10 @@ import mongoose from "mongoose"
 
 export default async function dbConnect() {
 	try {
-		const connectURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`
-		console.log(connectURI)
+		const encodedUser = encodeURIComponent(process.env.DB_USER!)
+		const encodedPass = encodeURIComponent(process.env.DB_PASSWORD!)
+		const connectURI = `mongodb://${encodedUser}:${encodedPass}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`
+
 		await mongoose.connect(connectURI)
 		console.log("MongoDB Connected Successfully")
 	} catch (e) {
