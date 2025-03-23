1e0bf3a00a131b70a7086da52e02c44c6b886249
vitoffer <ms.titoff1011@yandex.ru>
2025-03-23 11:52:39 +0300

    delete unused components

--- frontend/src/modules/trackList.ts
+++ frontend/src/modules/trackList.ts
@@ -1,18 +1,4 @@
-import { trackList } from "@/storage/storage"
 import { MixTrack, OriginalTrack } from "@/types"
-
-// export async function loadAllTracks() {
-// 	try {
-// 		const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/tracks`)
-// 		const tracks = await response.json()
-
-// 		tracks.forEach((track) => {
-// 			trackList.push(track)
-// 		})
-// 	} catch (err) {
-// 		console.error(err)
-// 	}
-// }
 
 export function getFilteredTrackList(
 	trackList: MixTrack[] | OriginalTrack[],
@@ -22,16 +8,3 @@ export function getFilteredTrackList(
 		return track.title.toLowerCase().includes(searchText.toLowerCase())
 	})
 }
-
-// function trackContainsText(track, text) {
-// 	return (
-// 		track.name.toLowerCase().includes(text.toLowerCase()) ||
-// 		track.authors.some((author) =>
-// 			author.toLowerCase().includes(text.toLowerCase()),
-// 		)
-// 	)
-// }
-
-// export function hasYoutubeLink(platforms) {
-// 	return platforms.youtube ?? false
-// }
