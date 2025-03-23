0298ffc17888bc6cce8d9bb8a987696d680b3dfb
vitoffer <ms.titoff1011@yandex.ru>
2025-03-23 11:23:33 +0300

    add original track list in track page and fix mobile layout

--- frontend/src/views/TrackPage.vue
+++ frontend/src/views/TrackPage.vue
@@ -1,46 +1,67 @@
 <script setup lang="ts">
 import BackButton from "@/components/BackButton.vue"
 import PlatformLinkList from "@/components/PlatformLinkList.vue"
+import TrackItem from "@/components/TrackItem.vue"
 import { trackList } from "@/storage/storage"
-import { Track } from "@/types"
-import { ref, Ref } from "vue"
+import { MixTrack, Track } from "@/types"
+import { ref, Ref, watch } from "vue"
 import { useRoute } from "vue-router"
-
-const route = useRoute()
-console.log(route.params)
 
 const track: Ref<Track | null> = ref(null)
 
-loadTrack()
-
-async function loadTrack() {
-	const data = trackList.value.find((track) => track.id === route.params.id)
+async function loadTrack(id: string) {
+	const data = trackList.value.find((track) => track.id === id)
 
 	track.value = data ?? null
 }
+
+const route = useRoute()
+
+watch(
+	() => route.params.id,
+	(newId) => {
+		loadTrack(newId as string)
+	},
+	{ immediate: true },
+)
 </script>
 
 <template>
 	<BackButton />
-	<div>
-		<div
-			v-if="track"
-			class="flex flex-col items-center gap-3"
-		>
+	<div
+		v-if="track"
+		class="flex flex-col gap-5"
+	>
+		<div class="flex flex-col items-center gap-3">
 			<img
 				:src="track.thumbnailUrl"
 				alt="Картинка трека"
 				class="aspect-square w-[250px] rounded-[10px]"
 			/>
 			<div class="flex flex-col gap-2">
-				<h1 class="text-center text-xl font-bold text-yellow-900">
+				<h1 class="text-center text-xl leading-5 font-bold text-yellow-900">
 					{{ track.title }}
 				</h1>
-				<p class="text-center text-lg text-yellow-700">
+				<p class="text-center text-lg leading-5 text-yellow-700">
 					{{ track.artistsNames.join(", ") }}
 				</p>
 			</div>
 			<PlatformLinkList :urls="track.urls" />
+		</div>
+		<div
+			v-if="track.isMix"
+			class="flex flex-col gap-3"
+		>
+			<p class="text-center text-cyan-700">Оригиналы:</p>
+			<ul class="track-list">
+				<li
+					v-for="track in (track as MixTrack).originalTracks"
+					:key="track.id"
+					class="not-first:-translate-y-[1px]"
+				>
+					<TrackItem :track="track" />
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
