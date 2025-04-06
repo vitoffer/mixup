372805a8c9231bbb8a060198b79b64eeba06a452
vitoffer <ms.titoff1011@yandex.ru>
2025-04-06 17:49:47 +0300

    Revert "increase z index of app nav elem"
    This reverts commit 98921b1f8a2062ff3845c786a78676a67652b5c6.

--- frontend/src/components/AppNav.vue
+++ frontend/src/components/AppNav.vue
@@ -28,7 +28,7 @@ function isActive(linkName: string) {
 
 <template>
 	<nav
-		class="nav fixed bottom-[env(safe-area-inset-bottom)] left-0 z-10000 w-full rounded-t-xl bg-gray-800 py-2"
+		class="nav fixed bottom-[env(safe-area-inset-bottom)] left-0 w-full rounded-t-xl bg-gray-800 py-2"
 	>
 		<ul class="flex justify-center gap-[48px]">
 			<li v-for="link in links">
