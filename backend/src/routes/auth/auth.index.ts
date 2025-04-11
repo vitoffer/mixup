55974903116b149eb81e49d77b29c7bb4c503f1b
vitoffer <ms.titoff1011@yandex.ru>
2025-04-12 02:36:23 +0300

    add base path auth for auth routes

--- backend/src/routes/auth/auth.index.ts
+++ backend/src/routes/auth/auth.index.ts
@@ -4,6 +4,7 @@ import * as handlers from "./auth.handlers"
 import * as routes from "./auth.routes"
 
 const router = createRouter()
+	.basePath("/auth")
 	.openapi(routes.login, handlers.login)
 	.openapi(routes.registerModerator, handlers.registerModerator)
 
